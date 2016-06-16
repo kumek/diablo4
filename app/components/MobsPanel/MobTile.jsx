e028eadada724f2eab0fd2d892dfd9ada8b58897
Adrian <kuma.adrian@gmail.com>
2016-06-16 09:47:16 +0000

    Colors for hp taking damage

--- app/components/MobsPanel/MobTile.jsx
+++ app/components/MobsPanel/MobTile.jsx
@@ -4,28 +4,55 @@ function(React, Assets) {
     var MobTile = React.createClass({displayName: 'MobTile',
     
         render: function() {
+            var progressValue = this.props.mob.hp/this.props.mob.maxhp;
+            
+            var progressColor = function(_value) {
+                if(_value < 0.3) {
+                    return '#A1110A';
+                }
+                if(_value < 0.6) {
+                    return '#E58827';
+                }
+                
+                if(_value < 0.8) {
+                    return '#FFCC00';
+                }
+                return '#1E9153';
+            }(progressValue);
+            
             return (
                 <div className="mobs-tile" style={{backgroundImage: 'url(' + Assets.monsters[this.props.mob.name] + ')'}}
                     onClick={this.props.onMobClick.bind(null, this.props.mob)}>
-                    <div className="stats">
-                        <span className="stats-attack">
-                            {this.props.mob.attack}
+                    {this.props.mob.hp === 0 ? 
+                        <span className="mob-dead">
                         </span>
-                        <span className="stats-defence">
-                            {this.props.mob.defence}
-                        </span>
+                    : 
+                    <div className="mob-alive">
+                        <div className="stats">
+                            <span className="stats-attack">
+                                {this.props.mob.attack}
+                            </span>
+                            <span className="stats-defence">
+                                {this.props.mob.defence}
+                            </span>
+                        </div>
+                        <div className="hp-bar progress">
+                                <div 
+                                    className="progress-bar"
+                                    role="progressbar" 
+                                    aria-valuenow={this.props.mob.hp} 
+                                    aria-valuemin="0" 
+                                    aria-valuemax={this.props.mob.maxhp} 
+                                    style={{
+                                        width: (100*progressValue)+'%',
+                                        backgroundColor: progressColor
+                                    }}>
+                                </div>
+                            
+                        </div>
                     </div>
-                    <div className="hp-bar progress">
-                            <div 
-                                className="progress-bar"
-                                role="progressbar" 
-                                aria-valuenow={this.props.mob.hp} 
-                                aria-valuemin="0" 
-                                aria-valuemax={this.props.mob.maxhp} 
-                                style={{width: 100*(this.props.mob.hp/this.props.mob.maxhp)+'%'}}>
-                            </div>
-                        
-                    </div>
+                    }
+                    
                     
                 </div>
             )
