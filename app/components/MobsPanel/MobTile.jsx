8411d5dd6f6e43c9aef2e3cc52b8b74aa4390c8a
Adrian <kuma.adrian@gmail.com>
2016-06-16 10:10:29 +0000

    Red mark on dead monster

--- app/components/MobsPanel/MobTile.jsx
+++ app/components/MobsPanel/MobTile.jsx
@@ -24,36 +24,31 @@ function(React, Assets) {
                 <div className="mobs-tile" style={{backgroundImage: 'url(' + Assets.monsters[this.props.mob.name] + ')'}}
                     onClick={this.props.onMobClick.bind(null, this.props.mob)}>
                     {this.props.mob.hp === 0 ? 
-                        <span className="mob-dead">
+                        <span className="mob-dead glyphicon glyphicon-remove">
+                        </span> 
+                    : null }
+                    
+                    <div className="stats">
+                        <span className="stats-attack">
+                            {this.props.mob.attack}
                         </span>
-                    : 
-                    <div className="mob-alive">
-                        <div className="stats">
-                            <span className="stats-attack">
-                                {this.props.mob.attack}
-                            </span>
-                            <span className="stats-defence">
-                                {this.props.mob.defence}
-                            </span>
-                        </div>
-                        <div className="hp-bar progress">
-                                <div 
-                                    className="progress-bar"
-                                    role="progressbar" 
-                                    aria-valuenow={this.props.mob.hp} 
-                                    aria-valuemin="0" 
-                                    aria-valuemax={this.props.mob.maxhp} 
-                                    style={{
-                                        width: (100*progressValue)+'%',
-                                        backgroundColor: progressColor
-                                    }}>
-                                </div>
-                            
-                        </div>
+                        <span className="stats-defence">
+                            {this.props.mob.defence}
+                        </span>
                     </div>
-                    }
-                    
-                    
+                    <div className="hp-bar progress">
+                            <div 
+                                className="progress-bar"
+                                role="progressbar" 
+                                aria-valuenow={this.props.mob.hp} 
+                                aria-valuemin="0" 
+                                aria-valuemax={this.props.mob.maxhp} 
+                                style={{
+                                    width: (100*progressValue)+'%',
+                                    backgroundColor: progressColor
+                                }}>
+                            </div>
+                    </div>
                 </div>
             )
         }
